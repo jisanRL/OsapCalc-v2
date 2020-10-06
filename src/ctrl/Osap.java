b627463078ccee5d0925fab03d0edcaade71eb08
jisan reza <jisanlatif@gmail.com>
2020-10-06 10:34:08 -0400

    Final commit

--- src/ctrl/Osap.java
+++ src/ctrl/Osap.java
@@ -31,154 +31,139 @@ public class Osap extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		// task 7 (p-2), to respond to the client with a message  
-		response.setContentType("text/plain");
-		Writer resOut = response.getWriter();
-		resOut.write("\n");														// optional for new line 
-		resOut.write("Hello, World!\n");
-		
-		String clientIP = request.getRemoteAddr();								// client IP
-		resOut.write("Client IP: " + clientIP + "\n");
-		
-		int cPort = request.getLocalPort();										// client port   or  request.getServerPort();[check this one as well]
-		resOut.write("Client Port: " + cPort + "\n");
-		resOut.write("This IP has been flagged!\n");
-		
-		String cProtocol = request.getProtocol();		    					// protocol 
-		resOut.write("Client Protocol: " + cProtocol + "\n");
-		
-		String cMethod = request.getMethod();									// method 
-		resOut.write("Client Method: " + cMethod + "\n");
-		
-		
-		String clientQueryString = request.getQueryString();					// querystring   
-		String foo = request.getParameter("foo");								// [check this part]
-		resOut.write("Query String: " + clientQueryString + "\n");  			// [check this part
-		resOut.write("Query Param foo= " + foo + "\n");
-		
-//		check this part if its needed 
-		String uri = request.getRequestURI().toString(); 						// URI 
-		resOut.write("Request URI: " + uri + "\n");
-		
-		
-		String servletPath = request.getServletPath();							// servlet path [this reads the web.xml file]
-		resOut.write("Request Servlet Path: " + servletPath + "\n");
-		
-		ServletContext context = this.getServletContext();
-		resOut.write("---Application info---\n");
-		
-		String appName = this.getServletContext().getInitParameter("applicationName");					// application name
-		resOut.write("Application Name= " + appName + "\n");
-		
-		String applicant = this.getServletContext().getInitParameter("applicantName");					// applicant Name
-		resOut.write("Applicant Name= " + applicant + "\n");
-		
-		// input from web.xml [this are the built-in/hard-coded values]
-		double default_principal = Double.parseDouble(this.getServletContext().getInitParameter("principal"));  // this reads the web.xml file and returns the parameter value of the param-name
-		double default_interest = Double.parseDouble(this.getServletContext().getInitParameter("interest"));
-		double default_period =  Double.parseDouble(this.getServletContext().getInitParameter("period"));
-//		double default_gracePeriod =  Double.parseDouble(this.getServletContext().getInitParameter("grace period"));
-		
-		// task B: servlet retrieving data from the form, [user input the values]
-		double principal = Double.parseDouble(request.getParameter("principal"));
-		double userInterest = Double.parseDouble(request.getParameter("interest"));				
-		double period =  Double.parseDouble(request.getParameter("period")); 			// grace period   
-		
-		
-		double sPrincipal, sPeriod, dInterest, grace;
-		double fixedInterest = Double.parseDouble(this.getServletContext().getInitParameter("fixed interest"));  // fixed interest 
-		double gracePeriod = Double.parseDouble(this.getServletContext().getInitParameter("grace period"));
-		double graceInterest;
-		
-		double interest = fixedInterest + userInterest;
-		if (request.getParameter("inputGrace") == null) {
-			graceInterest = 0;
-		} else {
-			graceInterest = principal * ((interest / 100)/12) * gracePeriod;
-		}
-		
-		// input from query String [these are the user input values]
-		if (request.getParameterMap().isEmpty()) {
-			 sPrincipal = default_principal;    		// the default values 
-			 sPeriod = default_period; 							
-			 dInterest = default_interest;		
-		} else {
-			 sPrincipal = principal;					// the user values
-			 sPeriod = period;
-			 dInterest = interest;     					// check this part again 
-		}
-
-//		check this part if its needed 
-		String contextPath = context.getContextPath();								// context path 
-		resOut.write("Context Path= " + contextPath + "\n");
-		
-		String realPath = context.getRealPath("Osap");				  				// real path
-		resOut.write("Real Path= " + realPath + "\n");
-		
-
-		// task D osap monthly payments, make parameters period, interest in web.xml 		
-		resOut.write("Based on Principal = " + sPrincipal);
-		resOut.write(" period=" + sPeriod);
-		resOut.write(" Interest=" + dInterest +"\n");
-		
-		
-		// the formula for osap calculation 
-		double payment; 
-		double calc = (((interest/100) / 12) * principal) / (1 - Math.pow(1 + ((interest/100) / 12), -sPeriod));
-	
-		if (request.getParameter("inputGrace") != null ) {
-			payment = calc + (graceInterest/gracePeriod);
-		} else {
-			payment = calc;
-		}
-		resOut.write("Monthly payments: " + payment);
-		
-		// task E : save session, how do you get the data from servlet into the results page
-		HttpSession session = request.getSession();
-		request.getServletContext().setAttribute("GI", graceInterest);
-		request.getServletContext().setAttribute("PAY", payment);
-		
-		
-		request.getSession().setAttribute("GI", graceInterest);			
-		request.getSession().setAttribute("PAY", payment);
-
-		String target = "/UI.jspx"; 				// this sets the path to UI.jspx instead of Osap.java
-		String resultPage = "/Results.jspx";
-
-		// task B and E to distinguish between a fresh visit and a submission visit
 		if (request.getParameter("calculate") == null || (request.getParameter("restart") != null && request.getParameter("restart").equals("true"))) {
 			request.getRequestDispatcher("/UI.jspx").forward(request, response);
 			
 		} else {
-			request.getRequestDispatcher("/Results.jspx").forward(request, response); 
+			response.getWriter().append("Served at: ").append(request.getContextPath());
+
+			response.setContentType("text/plain");
+			Writer resOut = response.getWriter();
+			resOut.write("\n"); // optional for new line
+			resOut.write("Hello, World!\n");
+
+			String clientIP = request.getRemoteAddr(); // client IP
+			resOut.write("Client IP: " + clientIP + "\n");
+
+			int cPort = request.getLocalPort(); // client port or request.getServerPort();
+			resOut.write("Client Port: " + cPort + "\n");
+			resOut.write("This IP has been flagged!\n");
+
+			String cProtocol = request.getProtocol(); // protocol
+			resOut.write("Client Protocol: " + cProtocol + "\n");
+
+			String cMethod = request.getMethod(); // method
+			resOut.write("Client Method: " + cMethod + "\n");
+
+			String clientQueryString = request.getQueryString(); // querystring
+			String foo = request.getParameter("foo"); 
+			resOut.write("Query String: " + clientQueryString + "\n"); 
+			resOut.write("Query Param foo= " + foo + "\n");
+
+
+			String uri = request.getRequestURI().toString(); // URI
+			resOut.write("Request URI: " + uri + "\n");
+
+			String servletPath = request.getServletPath(); // servlet path [this reads the web.xml file]
+			resOut.write("Request Servlet Path: " + servletPath + "\n");
+
+			ServletContext context = this.getServletContext();
+			resOut.write("---Application info---\n");
+
+			String appName = this.getServletContext().getInitParameter("applicationName"); // application name
+			resOut.write("Application Name= " + appName + "\n");
+
+			String applicant = this.getServletContext().getInitParameter("applicantName"); // applicant Name
+			resOut.write("Applicant Name= " + applicant + "\n");
+
+			/* 
+			 * input from web.xml [this are the built-in/hard-coded values]
+			 * these reads the web.xml file and returns the parameter value of the param-name
+			 * */
+
+			double default_principal = Double.parseDouble(this.getServletContext().getInitParameter("principal")); 																									 
+			double default_interest = Double.parseDouble(this.getServletContext().getInitParameter("interest"));
+			double default_period = Double.parseDouble(this.getServletContext().getInitParameter("period"));
+
+
+			// task B: servlet retrieving data from the form, [user input the values]
+			double principal = Double.parseDouble(request.getParameter("principal"));
+			double userInterest = Double.parseDouble(request.getParameter("interest"));
+			double period = Double.parseDouble(request.getParameter("period")); // grace period
+
+			double sPrincipal, sPeriod, dInterest, grace;
+			double fixedInterest = Double.parseDouble(this.getServletContext().getInitParameter("fixed interest")); // fixed interest
+			double gracePeriod = Double.parseDouble(this.getServletContext().getInitParameter("grace period"));
+			double graceInterest;
+
+			double interest = fixedInterest + userInterest;
+			if (request.getParameter("inputGrace") == null) {
+				graceInterest = 0;
+			} else {
+				graceInterest = principal * ((interest / 100) / 12) * gracePeriod;
+			}
+
+			// input from query String [these are the user input values]
+			if (request.getParameterMap().isEmpty()) {
+				sPrincipal = default_principal; // the default values
+				sPeriod = default_period;
+				dInterest = default_interest;
+			} else {
+				sPrincipal = principal; // the user values
+				sPeriod = period;
+				dInterest = interest; 
+			}
+
+			String contextPath = context.getContextPath(); // context path
+			resOut.write("Context Path= " + contextPath + "\n");
+
+			String realPath = context.getRealPath("Osap"); // real path
+			resOut.write("Real Path= " + realPath + "\n");
+
+			// task D osap monthly payments, make parameters period, interest in web.xml
+			resOut.write("Based on Principal = " + sPrincipal);
+			resOut.write(" period=" + sPeriod);
+			resOut.write(" Interest=" + dInterest + "\n");
+
+			// the formula for osap calculation
+			double payment;
+			double calc = (((interest / 100) / 12) * principal) / (1 - Math.pow(1 + ((interest / 100) / 12), -sPeriod));
+
+			if (request.getParameter("inputGrace") != null) {
+				payment = calc + (graceInterest / gracePeriod);
+			} else {
+				payment = calc;
+			}
+			resOut.write("Monthly payments: " + payment);
+
+			// task E : save session, how do you get the data from servlet into the results page
+			HttpSession session = request.getSession();
+			request.getServletContext().setAttribute("GI", graceInterest);
+			request.getServletContext().setAttribute("PAY", payment);
+
+			request.getSession().setAttribute("GI", graceInterest);
+			request.getSession().setAttribute("PAY", payment);
+
+			request.getRequestDispatcher("/Results.jspx").forward(request, response);
+			
+//			debugs 
+			System.out.println("default_principal = " + default_principal);
+			System.out.println("default_period =  " + default_period);
+			System.out.println("default_interest =  " + default_interest);
+			System.out.println("principal " + principal);
+			System.out.println("period " + period);
+			System.out.println("user interest " + userInterest);
+			System.out.println("------------------------------------------");
+			System.out.println("Grace Period = " + gracePeriod);
+			System.out.println("Grace Interest =  " + graceInterest);
+			System.out.println("------------------------------------------");
+			System.out.println("------------------------------------------");
+			System.out.println("Final calc: " + payment);
+			System.out.println("------------------------------------------");
+			System.out.println("Content length = " + request.getContentLength() + "\n" + "Content type = " + request.getContentType());
+			System.out.println("Hello, Got a " + cMethod + " request from Osap!");    
+		
 		} 
-		
-		
-		
-//		debugs 
-		System.out.println("default_principal = " + default_principal);
-		System.out.println("default_period =  " + default_period);
-		System.out.println("default_interest =  " + default_interest);
-		
-		System.out.println("principal " + principal);
-		System.out.println("period " + period);
-		System.out.println("user interest " + userInterest);
-		System.out.println("------------------------------------------");
-		System.out.println("Grace Period = " + gracePeriod);
-		System.out.println("Grace Interest =  " + graceInterest);
-//		System.out.println("Grace Interest =  " + default_gracePeriod);
-		System.out.println("------------------------------------------");
-//		System.out.println("target and request = " + request);
-//		System.out.println("Fixed interst = " + fixedInterest);
-//		System.out.println("Overall interest = " + overallInterest);
-		System.out.println("------------------------------------------");
-		System.out.println("Final calc: " + payment);
-		System.out.println("------------------------------------------");
-		System.out.println("Content length = " + request.getContentLength() + "\n" + "Content type = " + request.getContentType());
-//		System.out.println("Restart; " + restart);
-		System.out.println("Hello, Got a " + cMethod + " request from Osap!");    // task7
 	}
 
 	/**
